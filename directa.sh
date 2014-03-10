bf13532e3965eced958935221bcd150b42000297
Juli Mestieri, Projecte Ictineo SCCL <juli@projecteictineo.com>
2014-03-10 10:41:47 +0100

    correccions .make i script

--- directa.sh
+++ directa.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 rm -rf sites/all/modules/contrib
 rm -rf sites/all/themes/contrib
-drush make --working-copy --no-core --contrib-destination=. directa.make .
+rm -rf sites/all/libraries
+#drush make --working-copy --no-core --contrib-destination=. directa.make .
+drush make --working-copy --no-core directa.make .
 drush updatedb -y && drush cc all
 
 sudo chown 33:33 . -R
