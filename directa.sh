0396c4c8db7d9fce67afb93e335c4ff80060456f
benit3z <benitez@projecteictineo.com>
2014-03-13 12:14:09 +0100

    Comentar actualizacion bd make

--- directa.sh
+++ directa.sh
@@ -6,7 +6,8 @@ rm -rvf sites/all/themes/contrib
 rm -rvf sites/all/libraries
 #drush make --working-copy --no-core --contrib-destination=. directa.make .
 drush make --working-copy --no-core directa.make .
-drush updatedb -y && drush cc all
+#drush updatedb -y && 
+drush cc all
 
 sudo chown 33:33 . -R
 sudo chmod 775 . -R
