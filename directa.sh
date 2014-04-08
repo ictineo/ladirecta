fe47fc898a6ec1261f6c8664ed6cec630cb6ba3f
benit3z <benitez@projecteictineo.com>
2014-04-08 13:22:56 +0200

    Dashboard disabled

--- directa.sh
+++ directa.sh
@@ -5,6 +5,7 @@ rm -rvf sites/all/libraries
 #drush make --working-copy --no-core --contrib-destination=. directa.make .
 drush make --working-copy --no-core directa.make .
 #drush updatedb -y && 
+drush dis dashboard -y 
 drush dis overlay -y && drush cc all
 
 sudo chown 33:33 . -R
