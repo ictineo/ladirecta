0c41d324d85f327af3a4724c3d9493cf0fcf22a2
benit3z <benitez@projecteictineo.com>
2014-07-09 13:26:18 +0200

    Actualitzant script devel

--- drushdevel.sh
+++ drushdevel.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Generant continguts"
+
+drush en drush_devel
+echo "Generador contingut habilitat"
 
 drush genc 20 --types=cd_serveis,cd_noticies,faldo,butlleti_sonor,numero_paper,noticia,linies_de_recerca,cd_noticies --kill
 
