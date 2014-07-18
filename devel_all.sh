59003d826a9f316950ff10db1e3a74a999f4f35d
benit3z <benitez@projecteictineo.com>
2014-07-18 11:52:52 +0200

    Updating feature noticia

--- devel_all.sh
+++ devel_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-drush genc 50 --types=butlleti_sonor,numero_paper,noticia,fotogaleria,video,actualitat --kill
+drush genc 50 --types=butlleti_sonor,numero_paper,noticia,fotogaleria,video,actualitat,documents --kill
 
 echo "Script ejecutado"
