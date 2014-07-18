5ab239ea01b98b9a0dcca48cea28cafb4d748096
joangi <joan@projecteictineo.com>
2014-07-18 18:32:54 +0200

    context realead de la part del Joan

--- directa.make
+++ directa.make
@@ -11,6 +11,8 @@ projects[devel][version] = "1.3"
 projects[devel][subdir] = "contrib/dev"
 projects[features][version] = "2.0"
 projects[features][subdir] = "contrib/dev"
+projects[features_extra][version] = "1.0-beta1"
+projects[features_extra][subdir] = "contrib/dev"
 projects[strongarm][version] = "2.0"
 projects[strongarm][subdir] = "contrib/dev"
 
--- drushdevel.sh
+++ drushdevel.sh
@@ -9,7 +9,7 @@ drush gent tags 40
 
 
 
-drush genc 70 --types=cd_serveis,cd_noticies,faldo,butlleti_sonor,numero_paper,noticia,linies_de_recerca,cd_noticies,esdeveniment_agenda --kill
+drush genc 70 --types=cd_serveis,cd_noticies,faldo,butlleti_sonor,numero_paper,noticia,linies_de_recerca,cd_noticies,esdeveniment_agenda,fotogaleria,video --kill
 
 
 echo "Script ejecutado"
