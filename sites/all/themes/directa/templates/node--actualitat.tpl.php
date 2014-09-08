12a12c3515dc737a1e02b02193376efaa0e50926
root <root@directadev.ovh.net>
2014-09-08 18:28:36 +0200

    Joangi: avanant

---
+++ sites/all/themes/directa/templates/node--actualitat.tpl.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a node.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728164
+ */
+dsm($node);
+$query_next = new EntityFieldQuery();
+$query_next->entityCondition('entity_type', 'node')
+    ->entityCondition('bundle', 'actualitat')
+    ->propertyCondition('status', 1)
+    //->fieldCondition('field_data_ag', 'value', $node->field_data_ag[LANGUAGE_NONE][0]['value'], '>')
+    //->fieldOrderBy('field_data_ag', 'value', 'ASC')
+    ->range(0, 1);
+$result = $query_next->execute();
+$next_url   = (drupal_get_path_alias('node/' . array_keys($result['node'])[0]));
+$next_title = node_load(array_values($result['node'])[0]->nid)->title;
+
+$query_prev = new EntityFieldQuery();
+$query_prev->entityCondition('entity_type', 'node')
+    ->entityCondition('bundle', 'actualitat')
+    ->propertyCondition('status', 1)
+    //->fieldCondition('field_data_ag', 'value', $node->field_data_ag[LANGUAGE_NONE][0]['value'], '<')
+    //->fieldOrderBy('field_data_ag', 'value', 'DESC')
+    ->range(0, 1);
+$result = $query_prev->execute();
+$prev_url = (drupal_get_path_alias('node/' . array_keys($result['node'])[0]));
+$prev_title = node_load(array_values($result['node'])[0]->nid)->title;
+
+
+
+?>
+  <style> #navigation {display: none;} </style>
+<article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
+    <header>
+      <?php print render($title_prefix); ?>
+      <?php if (!$page && $title): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($display_submitted): ?>
+        <p class="submitted">
+          <?php print $user_picture; ?>
+          <?php print $submitted; ?>
+        </p>
+      <?php endif; ?>
+
+      <?php if ($unpublished): ?>
+        <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
+      <?php endif; ?>
+    </header>
+  <?php endif; ?>
+
+  <div class='node-nav-links node-nav-links-prev'><a href="<?php print $prev_url; ?>"><?php print $prev_title; ?></a></div>
+  <div class='node-nav-links node-nav-links-next'><a href="<?php print $next_url; ?>"><?php print $next_title; ?></a></div>
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    print render($content);
+  ?>
+
+  <?php print render($content['links']); ?>
+
+  <?php print render($content['comments']); ?>
+
+</article>
